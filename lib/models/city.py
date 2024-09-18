47b6988ee3b2888cfe1a630df985b94c8cc45e0d
Alin Silcock <at.silcock@gmail.com>
2024-09-18 10:52:15 -0600

    adding properties

--- lib/models/city.py
+++ lib/models/city.py
@@ -11,21 +11,34 @@ class City:
 
     @property
     def name(self):
-        return self.name
-    
+        return self._name
+
     @name.setter
     def name(self, name):
-        if not isinstance(name, str): 
-            raise Exception ("Name must be a string")
-        self._name = name
+        if isinstance(name, str) and len(name):
+            self._name = name
+        else:
+            raise ValueError("Name must be a non-empty string")
 
     @property
     def city_population(self):
         return self._city_population
-    
+
     @city_population.setter
     def city_population(self, city_population):
-        if not isinstance(city_population, int):
-            raise Exception("Popluation must be a Integer")
-        self._city_population = city_population
+        if isinstance(city_population, int):
+            self._city_population = city_population
+        else:
+            raise ValueError("Population must be an integer")
+
+    @property
+    def state_id(self):
+        return self._state_id
+
+    @state_id.setter
+    def state_id(self, state_id):
+        if isinstance(state_id, int) and State.find_by_id(state_id):
+            self._state_id = state_id
+        else:
+            raise ValueError("state_id must reference a valid state in the database")
             
