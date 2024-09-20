03893d74183a2c2ff070d3e4ce8c8969b2bbcf39
Alin Silcock <at.silcock@gmail.com>
2024-09-20 17:42:25 -0600

    updating my helper functions and my cli functions. The idea is refactoring: 1) The helper functions focus on interacting with the class methods that I have created in city.py and state.py 2) the cli focuses on what the user sees

--- lib/cli.py
+++ lib/cli.py
@@ -1,7 +1,11 @@
-from models.state import State
-from models.city import City
+from helpers import (
+    view_all_states, 
+    add_a_state, 
+    delete_a_state, 
+    update_a_state, 
+    get_cities_by_state
 
-
+)
 
 def main(): 
     while True:
@@ -10,7 +14,6 @@ def main():
         print("")
         main_menu()
         try: 
-            print("")
             choice = input("Enter your choice: ")
             print("")
 
@@ -31,125 +34,6 @@ def main():
         except ValueError:
             print("Invalid input. Please try again.")
 
-
-
-def view_all_states():
-    all_states = State.get_all()
-    for i, state in enumerate(all_states, start=1):
-        print(f"{i}. {state.name} | Population {state.population} | Region {state.region}")
-
-def add_a_state(name, population, region):
-
-    name = input("Please enter a name: ")
-    region = input ("please enter a region: ")
-    
-    while True: 
-        try: 
-            population = int(input("Please enter the population"))
-            break
-        except ValueError:
-            print("Population must be an integer")
-    print("You have succsefully added the population")
-    
-    if isinstance(name, str):
-        print("name is a string")
-    else:
-        print("not a string")
-
-    if isinstance(region, str):
-        print("region is a string")
-    else:
-        print("region is not a string")
-    
-    state_instance = State(name, population, region)
-    state_instance.save()
-    print(f"State {name} has now beend saved")
-
-def delete_a_state(name):
-    print("")
-    all_states = State.get_all()
-    view_all_states()
-    print()
-
-    try:
-        delete_state = int(input("Please enter the state # that you would like to delete: "))
-        print(f"You have entered {name}")
-    except ValueError: 
-        print("Invalid selection. Please try again.")
-
-    state_by_name = all_states[delete_state -1]
-
-
-    if state_by_name:
-        state_by_name.delete()
-        print(f"The {name} has been deleted")
-        
-    else:
-        print("State not found")
-
-def update_a_state(name, region, population):
-    all_states = State.get_all()
-    view_all_states()
-    
-    try:
-        print("")
-        update_state = int(input("Which state would you like to Update?: "))
-        print("")
-        print(f"Follow Directions: ")
-        print("")
-    except ValueError:
-        ("Try Again")
-
-    selected_state = all_states[update_state - 1]
-
-    if selected_state:
-        
-        name = input(f"Provide updated name: ")
-        if name.isalpha():
-            print("You have updated the name")
-        else:
-            print("")
-            print("Invalid name. Please try again.")
-            print("")
-            return update_a_state(name,region, population)
-            print("")
-
-        
-        region = input(f"Please enter the new region: ")
-        if region.isalpha():
-            print("You have updated the region")
-        else:
-            print("")
-            print("Invalid region name. Please try again.")
-            print("")
-            return update_a_state(name,region, population)
-        print("")
-
-        try:
-            population = int(input("Please enter the new population: "))
-        except ValueError:
-            print("Population must be a number")
-            return
-
-        selected_state.name = name
-        selected_state.region = region
-        selected_state.population = population
-        selected_state.update()
-
-    else:
-        print("try again ")
-
-def get_cities_by_state(state_id):
-    all_cities =  State.get_all()
-    view_all_states()
-    
-    try:
-    # write code where the user is choosing on of the available states
-        select_state_to_view_cities = int(input("Choose the state which you would like to view the cities for: "))
-        all_cities[select_state_to_view_cities -1]
-    
-
-
 def exit_program():
     print("You are now leaving - Goodbye!")
 
@@ -159,9 +43,8 @@ def main_menu():
     print("2. Add a state")
     print("3. Delete a state")
     print("4. Update a state")
-    print("5. Select State to view it's cities")
-
-
+    print("5. Select State to view its cities")
+    stars()
 
 def stars():
     print("******************")
--- lib/helpers.py
+++ lib/helpers.py
@@ -1,8 +1,88 @@
-
-
-
+from models.state import State
+from models.city import City
 
 def view_all_states():
     all_states = State.get_all()
     for i, state in enumerate(all_states, start=1):
         print(f"{i}. {state.name} | Population {state.population} | Region {state.region}")
+
+def add_a_state(name, population, region):
+    name = input("Please enter a name: ")
+    region = input ("please enter a region: ")
+
+    while True: 
+        try: 
+            population = int(input("Please enter the population"))
+            break
+        except ValueError:
+            print("Population must be an integer")
+    
+    print("You have successfully added the population")
+
+    state_instance = State(name, population, region)
+    state_instance.save()
+    print(f"State {name} has now been saved")
+
+def delete_a_state(name):
+    all_states = State.get_all()
+    view_all_states()
+
+    try:
+        delete_state = int(input("Please enter the state # that you would like to delete: "))
+    except ValueError: 
+        print("Invalid selection. Please try again.")
+        return
+
+    state_by_name = all_states[delete_state -1]
+
+    if state_by_name:
+        state_by_name.delete()
+        print(f"The {name} has been deleted")
+    else:
+        print("State not found")
+
+def update_a_state(name, region, population):
+    all_states = State.get_all()
+    view_all_states()
+
+    try:
+        update_state = int(input("Which state would you like to update?: "))
+    except ValueError:
+        print("Invalid input. Please try again.")
+        return
+
+    selected_state = all_states[update_state - 1]
+
+    if selected_state:
+        name = input(f"Please enter the updated name: ")
+        region = input(f"Please enter the updated region: ")
+
+        try:
+            population = int(input("Please enter the updated population: "))
+        except ValueError:
+            print("Population must be a number")
+            return
+
+        selected_state.name = name
+        selected_state.region = region
+        selected_state.population = population
+        selected_state.update()
+
+    else:
+        print("State not found. Try again.")
+
+def get_cities_by_state(state_id):
+    all_states = State.get_all()
+    view_all_states()
+    try:
+        select_state_to_view_cities = int(input("Choose the state which you would like to view the cities for: "))
+    except ValueError:
+        print("Invalid input. Please try again.")
+        return
+
+    all_cities = City.get_cities_by_state(select_state_to_view_cities)
+    if all_cities:
+        for i, city in enumerate(all_cities, start=1):
+            print(f"{i}. {city.name} | Population: {city.city_population}")
+    else:
+        print(f"No cities found for this state.")
